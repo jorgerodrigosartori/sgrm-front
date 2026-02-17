fe928045601ba116af7464f00fc6c9364be59bf4
Jorge Rodrigo Sartori <jorge.sartori@gmail.com>
2026-02-17 16:38:51 -0300

    Gera endpoint para envio de email apos carga

--- src/global/api/EnderecoServidor.js
+++ src/global/api/EnderecoServidor.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "https://sgrm-back.onrender.com";
-//export const API_BASE_URL = "http://localhost:3000";
+//export const API_BASE_URL = "https://sgrm-back.onrender.com";
+export const API_BASE_URL = "http://localhost:3000";
